678c43a1ff8c999997ea5e8ec0fd0f22592c9d4c
Roberto Garcia Navarro <info@soulcodex.es>
2024-08-01 23:02:36 +0200

    Add CORS bundle to ensure requests origins

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     League\Tactician\Bundle\TacticianBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
